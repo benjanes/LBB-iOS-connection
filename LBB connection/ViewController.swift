67b880c97fec04ce0408aff0311d407f8e63f6ce
Ben Janes <brjanes@gmail.com>
2016-09-21 11:40:35 -0400

    First commit

---
+++ LBB connection/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LBB connection
+//
+//  Created by Benjamin Robert Janes on 9/21/16.
+//  Copyright © 2016 Benjamin Robert Janes. All rights reserved.
+//
+
+import UIKit
+import Bean_iOS_OSX_SDK
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-LBB connection/Pods-LBB connection-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_LBB_connectionVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_LBB_connectionVersionString[];
+
